5925ef598b37578a5edeb1487eaa57170caf4aa4
joeblowout <75676630+joeblowout@users.noreply.github.com>
2022-05-04 13:57:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,8 @@ curl -fsSL https://get.docker.com | bash -s docker
 apt install python3-pip -y
 apt install zip unzip
 bash <(wget -qO- https://git.io/JJYE0)
-sudo curl -L "https://github.com/docker/compose/releases/download/2.5.0/docker-compose-linux-aarch64" -o /usr/local/bin/compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-aarch64" -o /usr/local/bin/compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/compose
 chmod +x /usr/local/bin/compose
 cat > docker-compose.yml <<EOF
 version: "2.1"
